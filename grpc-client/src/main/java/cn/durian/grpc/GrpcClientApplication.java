c0cfa9839385c4f9aa2525b629aeb00582251453
Liuluhao <luhao.liu@cncommdata.cn>
2020-08-27 14:15:53 +0800

    complete established

---
+++ grpc-client/src/main/java/cn/durian/grpc/GrpcClientApplication.java
@@ -0,0 +1,13 @@
+package cn.durian.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcClientApplication.class, args);
+    }
+
+}
---
+++ grpc-server/src/main/java/cn/durian/grpc/GrpcServerApplication.java
@@ -0,0 +1,13 @@
+package cn.durian.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerApplication.class, args);
+    }
+
+}
